9ce681714dc95616cdb1872cdf569ada3dfaf69c
Kolby Kiernan <kolbykiernan@gmail.com>
2024-01-22 17:54:04 -0800

    removed borders

--- src/components/Background.js
+++ src/components/Background.js
@@ -15,11 +15,15 @@ function Background() {
         justifyContent: 'center',
     }
 
+    const noBorder = {
+        border:'none'
+    }
+
   return (
     <Row>
         <Col fluid className='custom-background-color' md={8} style={cardStyling}>
-            <Card  >
-                <Card.Body fluid className='custom-background-color' >
+            <Card style={noBorder}>
+                <Card.Body fluid className='custom-background-color'  >
                     <Card.Text >
                         <h1>Order groceries for delivery or pickup today</h1>
                         <p>Whatever you want from local stores, brought right to your door</p>
@@ -37,7 +41,7 @@ function Background() {
             </Card>
         </Col>
         <Col md={4} fluid className="custom" style={noPadding} >
-            <Card fluid className='background-image'>
+            <Card fluid className='background-image' style={noBorder}>
           
                     <Card.Img  variant="top" src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg" style={noPadding} />
                 
